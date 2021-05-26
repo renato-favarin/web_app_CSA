e15c713eff71c3a02fa5361ecd087dc0aa34b013
Renato da Nova Favarin <renato.favarin@gmail.com>
2021-05-26 20:32:42 -0300

    atualização da base e do aplicativo

--- src/app.py
+++ src/app.py
@@ -7,8 +7,13 @@ import matplotlib.pyplot as plt
 st.set_page_config(page_title= "CSA Pindorama", page_icon=':seedling:')
 
 #@st.cache(allow_output_mutation=True)
-def carrega_dados(caminho, aba):
-    dados_nutricao = pd.read_excel(caminho, sheet_name = aba)
+def carrega_dados(caminho, aba, ignora_primeira_linha_coluna = False):
+    if ignora_primeira_linha_coluna == True:
+        dados_nutricao = pd.read_excel(caminho, sheet_name = aba, skiprows = [0])
+        dados_nutricao = dados_nutricao.set_index("Unnamed: 0")
+    else:
+        dados_nutricao = pd.read_excel(caminho, sheet_name = aba)
+        
     return dados_nutricao
 
 
@@ -19,12 +24,12 @@ def grafico_comparativo(dados_nutricao, lista_alimentos, atributo):
     
     figura, ax = plt.subplots()
     ax = sns.barplot(data = df, x = df.index, y = atributo)
-    ax.set_title(f"{atributo} por 100g de alimento")
+    ax.set_title(f"{atributo} por 100g de alimento cru")
     ax.set(xlabel=None)
     
     max = 0
     for p in ax.patches:
-        ax.annotate("%.2f" % p.get_height(), (p.get_x() + p.get_width() / 2., p.get_height()),
+        ax.annotate("%.1f" % p.get_height(), (p.get_x() + p.get_width() / 2., p.get_height()),
                     ha='center', va='center', fontsize=11, color='gray', xytext=(0, 10),
                     textcoords='offset points')
         
@@ -35,23 +40,17 @@ def grafico_comparativo(dados_nutricao, lista_alimentos, atributo):
     return figura
 
 def gera_tabela(df, label):
+        df = df.sort_values(by='produto')
         df.index = [" "] * len(df)
-        df['na_terra'] = " "
-        df['na_cesta'] = " "
-        df['  '] =  " "
-        df.rename({'produto':label,'na_terra': '   ', 'na_cesta':' '}, axis='columns', inplace=True)
-        st.table(df[[label, '   ', '  ',' ']])
-        
+        df.loc[:,'na_terra'] = " "
+        df.loc[:,'na_cesta'] = " "
+        df.loc[:, ' '] = " "
+        df.rename({'produto':label,'na_terra': '   ', 'na_cesta':'  '}, axis='columns', inplace=True)
+        st.table(df[[label, '   ', '  ', ' ']])   
         
 def main():
     
-    # por enquanto, não vou usar o google drive
-    #url_dados_csa = ' https://drive.google.com/file/d/1ZYFIPGIbQ52g1lKYHGhIGIBhT9ZMzB71/view?usp=sharing'
-    #caminho_dados_csa = 'https://drive.google.com/uc?export=download&id='+url_dados_csa.split('/')[-2]
-    #dados_csa = pd.read_excel(caminho_dados_csa, engine='openpyxl')
     caminho_dados_csa = "data/csa_pindorama.xlsx"
-    
-    
     dados_csa = carrega_dados(caminho_dados_csa, 'produtos') 
     
     st.title("CSA Pindorama :seedling:")
@@ -81,25 +80,27 @@ def main():
     
     st.markdown("**Agenda:** :car: :herb:")
     if st.checkbox("Busca cesta", value=False):
-        dados_busca_cesta = carrega_dados(caminho_dados_csa, 'busca_cesta')
-        dados_busca_cesta.index = ["carro_1", "carro_2"]
+        dados_busca_cesta = carrega_dados(caminho_dados_csa, 'busca_cesta', ignora_primeira_linha_coluna = True)
         st.table(dados_busca_cesta)
     
     if st.checkbox("Mutirões", value=False):
-        dados_mutiroes = carrega_dados(caminho_dados_csa, 'mutiroes')
-        dados_mutiroes.index = ["objetivo", "presença_confirmada"]
+        
+        dados_mutiroes = carrega_dados(caminho_dados_csa, 'mutiroes', ignora_primeira_linha_coluna = True)
         st.table(dados_mutiroes)
+        st.markdown("**Atenção:** uso obrigatório de máscara e álcool gel/70% durante todo o mutirão.")
     
     
     st.write("-------------------------------------------")
     
     st.markdown("**Extra:** :books: :telephone:")
-    if st.checkbox("Informações nutricionais dos alimentos da CSA (em construção)", value=False):
+    if st.checkbox("Informações nutricionais dos alimentos da CSA", value=False):
         dados_nutricao = carrega_dados(caminho_dados_csa, 'info_nutricional')
         info_alimento = dados_nutricao["alimento"].values
         info_nutricional = dados_nutricao.columns[1:]
 
-        opcao_info_nutricional = st.radio("informação nutricional (a cada 100g de alimento):",info_nutricional)
+        opcao_info_nutricional = st.radio("informação nutricional (a cada 100g de alimento cru):",info_nutricional)
+        
+        lugar_grafico = st.empty()
         
         opcao_alimento_1 = st.selectbox("alimento:",info_alimento, index = 0)
         opcao_alimento_2 = st.selectbox("  alimento:",info_alimento, index = 3)
@@ -110,8 +111,8 @@ def main():
                                                      opcao_alimento_2,
                                                      opcao_alimento_3, 
                                                      opcao_alimento_4], opcao_info_nutricional)
+        lugar_grafico.pyplot(figura) 
         
-        st.pyplot(figura)
         st.markdown("Fonte principal dos dados: [Escola Paulista de Medicina](http://tabnut.dis.epm.br/) :school:")
         
     if st.checkbox("Contatos", value=False):
